77ae69632a394246b6e8cdff479949eaad36ffe4
Charles Stapleton <stapletc@hotmail.com>
2018-10-25 18:40:43 -0700

    scaffolding DOM

--- js/app.js
+++ js/app.js
@@ -1,3 +1,41 @@
 ('use strict');
 
+var restaurantName = document.createElement('h2');
+restaurantName.textContent = 'Restaurant Name';
+document.body.appendChild(restaurantName);
 
+var restaurantCuisine = document.createElement('p');
+restaurantCuisine.textContent = 'Restaurant Cuisine';
+document.body.appendChild(restaurantCuisine);
+
+var restaurantDescription = document.createElement('p');
+restaurantDescription.textContent = 'Restaurant Description';
+document.body.appendChild(restaurantDescription);
+
+var restaurantAddress = document.createElement('p');
+restaurantAddress.textContent = 'Restaurant Address';
+document.body.appendChild(restaurantAddress);
+
+var restaurantDistance = document.createElement('p');
+restaurantDistance.textContent = 'Restaurant Distance';
+document.body.appendChild(restaurantDistance);
+
+var restaurantHours = document.createElement('p');
+restaurantHours.textContent = 'Restaurant Hours';
+document.body.appendChild(restaurantHours);
+
+var restaurantPhone = document.createElement('p');
+restaurantPhone.textContent = 'Restaurant Phone';
+document.body.appendChild(restaurantPhone);
+
+var restaurantWebsite = document.createElement('p');
+restaurantWebsite.textContent = 'Restaurant Website';
+document.body.appendChild(restaurantWebsite);
+
+var restaurantCodability = document.createElement('p');
+restaurantCodability.textContent = 'Restaurant Codability';
+document.body.appendChild(restaurantCodability);
+
+var restaurantMenu = document.createElement('p');
+restaurantMenu.textContent = 'Restaurant Menu';
+document.body.appendChild(restaurantMenu);
